2b7185048ed938e868bd6c0a95e5888270a344ca
jartek <jartek.91@gmail.com>
2014-11-30 11:44:24 +0530

    Selection sort

---
+++ sorting/selection_sort/selection_sort.go
@@ -0,0 +1,20 @@
+package selection_sort
+
+func Swap(min *float64, current *float64) {
+	tmp := *min
+	*min = *current
+	*current = tmp
+}
+
+func Sort(input []float64) []float64 {
+	for i := 0; i < len(input); i++ {
+		min := i
+		for j := i + 1; j < len(input); j++ {
+			if input[j] < input[min] {
+				min = j
+			}
+		}
+		Swap(&input[min], &input[i])
+	}
+	return input
+}
